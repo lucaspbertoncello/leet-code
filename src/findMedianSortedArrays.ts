5e6b5679645c5370c92873a2dbd03c09d82897c4
Lucas <lucasbertoncello1@gmail.com>
2025-05-27 16:17:48 -0300

    findMedianSortedArrays solved

--- src/findMedianSortedArrays.ts
+++ src/findMedianSortedArrays.ts
@@ -1,12 +1,22 @@
-const findMedianSortedArrays = (nums1: number[], nums2: number[]) => {
-  const mergeArr: number[] = [...nums1, ...nums2];
-
-  const sum: number = mergeArr.reduce(
-    (acc: number, item: number) => acc + item,
-    0
-  );
-
-  return sum / mergeArr.length;
+const isOdd = (num: number): boolean => {
+  if (num % 2 === 0) {
+    return false;
+  }
+  return true;
 };
 
-console.log(findMedianSortedArrays([2, 2, 4, 4], [2, 2, 2, 4, 4]));
+const findMedianSortedArrays = (nums1: number[], nums2: number[]) => {
+  const mergeArr: number[] = [...nums1, ...nums2];
+  const sortedArr = mergeArr.sort((a, b) => a - b);
+
+  if (isOdd(sortedArr.length)) {
+    return sortedArr[Math.floor(sortedArr.length / 2)];
+  }
+
+  const firstMid = sortedArr[Math.floor(sortedArr.length / 2)];
+  const secMid = sortedArr[Math.floor(sortedArr.length / 2 - 1)];
+
+  return (firstMid + secMid) / 2;
+};
+
+console.log(findMedianSortedArrays([4, 2], [3, 1, 4, 5, 6, 7, 8]));
---
+++ src/mergeTwoLists.ts
@@ -0,0 +1,13 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+const mergeTwoLists = (
+  l1: number[] | null,
+  l2: number[] | null
+): number[] | null => {
+  const newList: number[] = [...(l1 || []), ...(l2 || [])];
+  return newList.sort();
+};
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
